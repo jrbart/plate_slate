c53daebf016c831ba91d610dfe4f5f3d77376789
jrbart <jorabart@gmail.com>
2026-02-01 07:25:28 -0600

    Update 20260131210744_create_items_taggings.exs

--- priv/repo/migrations/20260131210744_create_items_taggings.exs
+++ priv/repo/migrations/20260131210744_create_items_taggings.exs
@@ -5,8 +5,6 @@ defmodule PlateSlate.Repo.Migrations.CreateItemsTaggings do
     create table(:items_taggings) do
       add :item_id, references(:items, on_delete: :nothing)
       add :item_tag_id, references(:item_tags, on_delete: :nothing)
-
-      timestamps(type: :utc_datetime)
     end
 
     create index(:items_taggings, [:item_id])
