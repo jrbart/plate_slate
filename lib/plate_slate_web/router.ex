92a42900a6c488a46e31cb5bfd14dfb6ffdbf832
jrbart <jorabart@gmail.com>
2026-02-01 07:37:01 -0600

    Squashed commit of the following:
    commit 8f3eba437a7641d806f36928ae8d7087c90cf1ef
    Author: jrbart <jorabart@gmail.com>
    Date:   Sun Feb 1 07:36:23 2026 -0600
    
        add graphQL schema and config router

--- lib/plate_slate_web/router.ex
+++ lib/plate_slate_web/router.ex
@@ -14,10 +14,15 @@ defmodule PlateSlateWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", PlateSlateWeb do
-    pipe_through :browser
+  scope "/" do
+    pipe_through :api
 
-    get "/", PageController, :home
+    forward "/api", Absinthe.Plug,
+      schema: PlateSlateWeb.Schema
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: PlateSlateWeb.Schema,
+      interface: :simple
   end
 
   # Other scopes may use custom stacks.
