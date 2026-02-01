d6df2e791bdb94b612bb332077a3cb00f97682e0
jrbart <jorabart@gmail.com>
2026-02-01 09:38:01 -0600

    Squashed commit of the following:
    commit 80a112cdfa8d699e0ca3b36aac03fa9883bff0f8
    Author: jrbart <jorabart@gmail.com>
    Date:   Sun Feb 1 09:35:47 2026 -0600
    
        add test for bad input value
    
    commit ba049cfffabe51712471e8cc45cf3f1bf179c6ec
    Author: jrbart <jorabart@gmail.com>
    Date:   Sun Feb 1 09:35:24 2026 -0600
    
        add filtering

--- test/plate_slate_web/schema/query/menu_items_test.exs
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -2,7 +2,7 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
   use PlateSlateWeb.ConnCase, async: true
 
   setup do
-    PlateSlate.Seeds.run() 
+    PlateSlate.Seeds.run()
   end
 
   @query """
@@ -13,30 +13,68 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
   }
   """
   test "menuItems field returns menu items" do
-    conn = build_conn() 
+    conn = build_conn()
     conn = get conn, "/api", query: @query
-    assert json_response(conn, 200) == %{ 
-      "data" => %{
-        "menuItems" => [
-          %{"name" => "Reuben"},
-          %{"name" => "Croque Monsieur"},
-          %{"name" => "Muffuletta"},
-          # Rest of items
-          %{"name" => "Bánh mì"},
-          %{"name" => "Vada Pav"},
-          %{"name" => "French Fries"},
-          %{"name" => "Papadum"},
-          %{"name" => "Pasta Salad"},
-          %{"name" => "Water"},
-          %{"name" => "Soft Drink"},
-          %{"name" => "Lemonade"},
-          %{"name" => "Masala Chai"},
-          %{"name" => "Vanilla Milkshake"},
-          %{"name" => "Chocolate Milkshake"},
-        ]
-      }
-    }
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"},
+                 %{"name" => "Croque Monsieur"},
+                 %{"name" => "Muffuletta"},
+                 # Rest of items
+                 %{"name" => "Bánh mì"},
+                 %{"name" => "Vada Pav"},
+                 %{"name" => "French Fries"},
+                 %{"name" => "Papadum"},
+                 %{"name" => "Pasta Salad"},
+                 %{"name" => "Water"},
+                 %{"name" => "Soft Drink"},
+                 %{"name" => "Lemonade"},
+                 %{"name" => "Masala Chai"},
+                 %{"name" => "Vanilla Milkshake"},
+                 %{"name" => "Chocolate Milkshake"}
+               ]
+             }
+           }
   end
 
-end
+  @query """
+  {
+    menuItems(matching: "reu") {
+      name
+    }
+  }
+  """
+  test "menuItems field returns menu items filtered by name" do
+    response = get(build_conn(), "/api", query: @query)
 
+    assert json_response(response, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"}
+               ]
+             }
+           }
+  end
+
+@query """
+{
+  menuItems(matching: 123) {
+    name
+  }
+}
+"""
+test "menuItems field returns errors when using a bad value" do
+  response = get(build_conn(), "/api", query: @query)
+
+  assert %{
+           "errors" => [
+             %{"message" => message}
+           ]
+         } = json_response(response, 200)
+
+  assert message ==
+           "Argument \"matching\" has invalid value 123."
+end
+end
