a0efe4b1fc74072b9e06d0858394f1ca9372f5fa
jrbart <jorabart@gmail.com>
2026-02-03 12:18:47 -0600

    add filtering and tests

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -7,10 +7,24 @@ defmodule PlateSlateWeb.Schema do
     value(:desc)
   end
 
+  @desc "Filtering options for menu item list"
+  input_object :menu_item_filter do
+    @desc "Matching a name"
+    field :name, :string
+    @desc "Matching a cotegory name"
+    field :category, :string
+    @desc "Matching a tag"
+    field :tag, :string
+    @desc "Priced above a value"
+    field :priced_above, :float
+    @desc "Priced below a value"
+    field :priced_below, :float
+  end
+
   query do
     @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
-      arg(:matching, :string)
+      arg(:filter, :menu_item_filter)
       arg(:order, :sort_order, default_value: :asc)
       resolve(&Resolvers.Menu.menu_items/3)
     end
