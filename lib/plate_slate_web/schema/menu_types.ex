7e9a1c6c80b749e9b39a4e330ed75b59c7a42b2b
jrbart <jorabart@gmail.com>
2026-02-05 06:16:55 -0600

    move menu_type related definitions into schema/menu_types.ex and import

---
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -0,0 +1,23 @@
+defmodule PlateSlateWeb.Schema.MenuTypes do
+  use Absinthe.Schema.Notation
+
+  @desc "Filtering options for menu item list"
+  input_object :menu_item_filter do
+    @desc "Matching a name"
+    field :name, :string
+    @desc "Matching a cotegory name"
+    field :category, :string
+    @desc "Matching a tag"
+    field :tag, :string
+    @desc "Priced above a value"
+    field :priced_above, :float
+    @desc "Priced below a value"
+    field :priced_below, :float
+  end
+
+  object :menu_item do
+    field :id, :id
+    field :name, :string
+    field :description, :string
+  end
+end
