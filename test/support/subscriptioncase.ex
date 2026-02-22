607efcc16233f816ad85dc90723168dd4b3fb7d1
jrbart <jorabart@gmail.com>
2026-02-22 06:34:13 -0600

    add subscriptioncase
    support file to set up socket for running subscription tests

---
+++ test/support/subscriptioncase.ex
@@ -0,0 +1,36 @@
+defmodule PlateSlateWeb.SubscriptionCase do
+  @moduledoc """
+  his module defines the test case to be used by
+  subscription tests
+  """
+  require Phoenix.ChannelTest
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      # Import conveniences for testing with channels
+      use PlateSlateWeb.ChannelCase
+
+      use Absinthe.Phoenix.SubscriptionTest,
+        shcmea: PlateSlateWeb.Schema
+
+      setup do
+        PlateSlate.Seeds.run()
+
+        {:ok, socket} = Phoenix.ChannelTest.connect(PlateSlateWeb.UserSocket, %{})
+
+        {:ok, socket} = Absinthe.Phoenix.SubscriptionTest.join_absinthe(socket)
+
+        {:ok, socket: socket}
+      end
+
+      import unquote(__MODULE__), only: [menu_item: 1]
+    end
+  end
+
+  # handy function for grabbing a fixture
+  def menu_item(name) do
+    PlateSlate.Repo.get_by!(PlateSlate.Menu.Item, name: name)
+  end
+end
