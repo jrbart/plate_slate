81cb57f358f6ca2a2f34f4a247899d75d35576bb
jrbart <jorabart@gmail.com>
2026-02-22 06:35:11 -0600

    add subscription test skeleton
    basic test file for subscriptions

---
+++ test/plate_slate_web/schema/subscription/new_order_test.exs
@@ -0,0 +1,17 @@
+defmodule PlateSlateWeb.Schema.Subscription.NewOrderTest do
+  use PlateSlateWeb.SubscriptionCase
+
+  @subscription """
+
+  """
+  @mutation """
+
+  """
+  test "", %{socket: socket} do
+    # setup a subscription
+
+    # make a mutation to trigger the subscription
+
+    # check for subscription data
+  end
+end
